8eef25ef0ac2a842e4aae1f7d135e461f6ece279
virpartap-singh <singhvirpartap@gmail.com>
2022-06-06 11:42:05 +0530

    fav remove

--- src/container/newsletter/Newsletter.tsx
+++ src/container/newsletter/Newsletter.tsx
@@ -48,7 +48,7 @@ const Newsletter:React.FC=():React.ReactElement=> {
   return (
     <Container>
     <Title>Newsletter</Title>
-    <Desc>Get timely updates from your favorite.</Desc>
+    <Desc>Get timely updates from your.</Desc>
     <InputContainer>
       <Input placeholder="Your email" />
       <Button>
